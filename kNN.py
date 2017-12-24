b71fb873fc8ac33b62cc586381cd39b00eba4018
thishzw@126.com <thishzw@126.com>
2017-12-24 18:33:12 +0800

    kNN算法

---
+++ kNN.py
@@ -0,0 +1,39 @@
+from numpy import *
+def classify0(inX,dataSet,labels,k):
+    dataSize=dataSet.shape[0]
+    diffMat=tile(inX,(dataSize,1))-dataSet#在列方向上重复inX次，行dataSize次
+    sqDiffMat=diffMat**2
+    sqDistances=sqDiffMat.sum(axis=1)#同一行相加
+    distances=sqDistances**0.5
+    sortedDistIndicies=distances.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel=labels[sortedDistIndicies[i]];
+        classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
+    sortedClassCount=sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr=open(filename)
+    arrayOLines=fr.readlines()
+    numberOfLines=len(arrayOLines)
+    returnMat=zeros((numberOfLines,3))
+    classLabelVector=[]
+    index=0
+    for line in arrayOLines:
+        line=line.strip()#去除首尾的空白字符
+        listFromLine=line.split('\t')
+        returnMat[index,:]=listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index+=1
+    return returnMat,classLabelVector
+    
+def autoNorm(dataSet):
+    minVals=dataSet.min(0)
+    maxVals=dataSet.max(0)
+    ranges=maxVals-minVals
+    normDataSet=zeros(shape(dataSet))
+    m=dataSet.shape[0]
+    normDataSet=dataSet-tile(minVals,(m,1))
+    normDataSet=normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
